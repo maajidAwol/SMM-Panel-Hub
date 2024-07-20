3eeb02e070bd8f66496ed6343b5012d6b7b33679
Abdulmajid Awol <maajidawol@gmail.com>
2024-07-21 02:11:46 +0300

    pages without content

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-// script.js
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
+// // script.js
+// const mobileMenu = document.getElementById('mobile-menu');
+// const navLinks = document.querySelector('.nav-links');
 
-mobileMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    if (navLinks.classList.contains('active')) {
-        navLinks.style.display = 'block';
+// mobileMenu.addEventListener('click', () => {
+//     navLinks.classList.toggle('active');
+//     if (navLinks.classList.contains('active')) {
+//         navLinks.style.display = 'block';
+//     } else {
+//         navLinks.style.display = 'none';
+//     }
+// });
+
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var menuButton = document.getElementById("menuButton");
+    var menuIcon = document.getElementById("menuIcon");
+    
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden");
+      menu.classList.add("block");
+      menuIcon.setAttribute("d", "M6 6l12 12");
     } else {
-        navLinks.style.display = 'none';
+      menu.classList.remove("block");
+      menu.classList.add("hidden");
+      menuIcon.setAttribute("d", "M6 18L18 6M6 6l12 12");
     }
-});
+  }
